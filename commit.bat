66efb419c72918251e4066b2b7eff57e0bf4df9d
nkcsgexi <xge@ncsu.edu>
2013-03-18 01:36:48 -0400

    auto batch commit

--- commit.bat
+++ commit.bat
@@ -1,5 +1,4 @@
 CALL cd "C:\programming\Eclipse_Workspace\ConditionPreComputing"
-CALL git add *.java *.xml *.jar
+CALL git add *.java *.xml *.jar *.bat
 CALL git commit -a -m "auto batch commit"
-(ECHO nkcsgexi 
- ECHO bxh022408) | git push origin
+ECHO nkcsgexi | CALL git push origin
