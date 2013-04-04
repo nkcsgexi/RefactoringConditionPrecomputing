f8c11949d86f55afec086e1eab06fb34747019bb
nkcsgexi <xge@ncsu.edu>
2013-04-03 23:00:34 -0400

    auto batch commit

---
+++ test/javamodel/tests/ConditionThreeExperiments.java
@@ -0,0 +1,77 @@
+package javamodel.tests;
+
+import org.eclipse.core.runtime.Assert;
+import org.junit.Test;
+
+import edu.ncsu.dlf.refactoring.precondition.util.MathUtils;
+
+public class ConditionThreeExperiments {
+	
+	private static final String CHAR_LIST = 
+	        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+	
+	private char getRandomChar()
+	{
+		  int number = MathUtils.getRondomInteger(0, CHAR_LIST.length() - 1);
+          return CHAR_LIST.charAt(number);
+	}
+	
+	private String generateRandomString(int length)
+	{
+        StringBuffer randStr = new StringBuffer();
+        for(int i=0; i<length; i++){
+            randStr.append(getRandomChar());
+        }
+        return randStr.toString();
+	}
+	
+	@Test
+	public void testGetRandomChar()
+	{
+		boolean lowerReached = false;
+		boolean upperReached = false;
+		for(int i = 0 ; i< 1000; i ++)
+		{
+			char c = getRandomChar();
+			if(c == 'a')
+				lowerReached = true;
+			if(c == '0')
+				upperReached = true;
+		}
+		Assert.isTrue(upperReached);
+		Assert.isTrue(lowerReached);
+	}
+	
+	private boolean verifyIdentifier() {
+		char[] identifier = generateRandomString(200).toCharArray();
+		if(Character.isJavaIdentifierStart(identifier[0]))
+		{
+			for(char c : identifier)
+			{
+				if(!Character.isJavaIdentifierPart(c))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	@Test
+	public void verifyIdentifiers()
+	{
+		for(int i = 0; i< 10000; i ++)
+			verifyIdentifier();
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+
+}
