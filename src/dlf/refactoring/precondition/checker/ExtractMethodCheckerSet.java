3d36054653ca4b6d7730fe2c57c739602229833e
nkcsgexi <xge@ncsu.edu>
2013-04-10 16:55:05 -0400

    auto batch commit

--- src/dlf/refactoring/precondition/checker/ExtractMethodCheckerSet.java
+++ src/dlf/refactoring/precondition/checker/ExtractMethodCheckerSet.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import org.eclipse.jdt.core.IJavaElement;
 
+import dlf.refactoring.enums.ConditionType;
+import dlf.refactoring.enums.InputType;
 import dlf.refactoring.enums.RefactoringType;
 import dlf.refactoring.precondition.JavaModelAnalyzers.ICompilationUnitAnalyzer;
 import dlf.refactoring.precondition.JavaModelAnalyzers.IMethodAnalyzer;
 import dlf.refactoring.precondition.JavaModelAnalyzers.ITypeAnalyzer;
 import dlf.refactoring.precondition.checker.environments.IRefactoringEnvironment;
+import dlf.refactoring.precondition.checker.environments.IRefactoringInput;
 import dlf.refactoring.precondition.checker.environments.RefactoringContext;
 import dlf.refactoring.precondition.checker.environments.SingleELementRefacatoringEnvironment;
+import dlf.refactoring.precondition.checker.environments.StringRefactoringInput;
+import dlf.refactoring.precondition.checker.result.C2CheckingResult;
 import dlf.refactoring.precondition.checker.result.ICheckingResult;
 import dlf.refactoring.precondition.util.XArrayList;
 import dlf.refactoring.precondition.util.interfaces.IConvertor;
@@ -34,8 +39,7 @@ public class ExtractMethodCheckerSet extends RefactoringCheckerSet {
 		XArrayList<IRefactoringEnvironment> allEnvironments = new XArrayList
 				<IRefactoringEnvironment>();
 		Iterator<IJavaElement> it = context.getCompilationUnits();
-		for( ;it.hasNext(); )
-		{
+		for( ;it.hasNext(); ) {
 			allEnvironments.addAll(ICompilationUnitAnalyzer.getTypes(it.next()).select(new 
 					IMapper<IJavaElement, IJavaElement>(){
 				@Override
@@ -54,8 +58,8 @@ public class ExtractMethodCheckerSet extends RefactoringCheckerSet {
 		return allEnvironments;
 	}
 	
-	private class ExtractMethodEnvironment extends SingleELementRefacatoringEnvironment
-	{
+	private class ExtractMethodEnvironment extends SingleELementRefacatoringEnvironment {
+		
 		public ExtractMethodEnvironment(IJavaElement element) {
 			super(element);
 		}
@@ -67,22 +71,72 @@ public class ExtractMethodCheckerSet extends RefactoringCheckerSet {
 	}
 	
 	
-	private class ExtractableStatementsChecker implements IConditionChecker
-	{
+	
+	private class ExtractableStatementsChecker implements IConditionChecker {
 		@Override
 		public ICheckingResult performChecking(IRefactoringEnvironment environment) throws Exception 
 		{
 			return null;
-		}	
+		}
+		
+		private class Statement2Extract extends C2CheckingResult
+		{
+			@Override
+			public ConditionType getConditionType() {
+				return ConditionType.EXTRACTABLE_STATEMENTS;
+			}	
+		}
+		
+		private class StartEndStatementInput implements IRefactoringInput
+		{
+			int start;
+			int end;
+			
+			@Override
+			public int compareTo(Object arg) {
+				
+				int otherStart = ((StartEndStatementInput)arg).start;
+				int otherEnd = ((StartEndStatementInput)arg).end;
+				if(start != otherStart)
+					return start - otherStart;
+				if(end != otherEnd)
+					return end - otherEnd;
+				return 0;
+			}
+
+			@Override
+			public InputType getInputType() {
+				return null;
+			}	
+		}
 	}
 	
 	
-	private class AvailableMethodNameChecker implements IConditionChecker
-	{
+	private class AvailableMethodNameChecker implements IConditionChecker{
 		@Override
 		public ICheckingResult performChecking(IRefactoringEnvironment environment) throws Exception 
 		{
 			return null;
+		}
+		
+		private class MethodNameChecker extends C2CheckingResult
+		{
+			@Override
+			public ConditionType getConditionType() {
+				return ConditionType.NAME_COLLISION;
+			}	
+		}
+		
+		private class MethodNameInput extends StringRefactoringInput
+		{
+			public MethodNameInput(String input) {
+				super(input);
+			}
+
+			@Override
+			public InputType getInputType() {
+				return InputType.NEW_NAME;
+			}	
 		}
 	}
 
